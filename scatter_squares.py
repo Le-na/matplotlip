d116ad5e54557326b9e6cdd294ac99af59bd283e
elena <zanozinaeo@gmail.com>
2023-07-21 17:12:30 +0300

    show one point

---
+++ scatter_squares.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+
+
+plt.style.use("seaborn-v0_8")    # Нанеесение на диаграмму отдельной точки используется функция scatter()
+fig, ax = plt.subplots()
+ax.scatter(2, 4, s=100)
+
+# Назначение заголовка диаграммы и меток осей.
+ax.set_title("Square Numbers", fontsize=24)
+ax.set_xlabel("Value", fontsize=14)
+ax.set_ylabel("Square of Value", fontsize=14)
+
+# Назначение размера шрифта делений на осях.
+ax.tick_params(axis='both', which='major', labelsize=14)
+
+plt.show()
