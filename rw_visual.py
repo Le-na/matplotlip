d4e6144fb8ce7d7eaabc50476487316db8e6b498
elena <zanozinaeo@gmail.com>
2023-07-23 18:36:01 +0300

    upgrade random graph

--- rw_visual.py
+++ rw_visual.py
@@ -1,12 +1,18 @@
 import matplotlib.pyplot as plt
 from random_walk import RandomWalk
 
+""" Новые блуждания строятся до ттех пор, пока программма остаетсяяя активной"""
 # Построение случайного блуждания
-rw = RandomWalk()
-rw.fill_walk()
+while True:
+    rw = RandomWalk()
+    rw.fill_walk()
 
-# Нанесение точек на диаграмму
-plt.style.use("classic")
-fig, ax = plt.subplots()
-ax.scatter(rw.x_values, rw.y_values, s=15)
-plt.show()
+    # Нанесение точек на диаграмму
+    plt.style.use("classic")
+    fig, ax = plt.subplots()
+    ax.scatter(rw.x_values, rw.y_values, s=15)
+    plt.show()
+
+    keep_running = input("Make another walk? (y/n):")
+    if keep_running == "n":
+        break
