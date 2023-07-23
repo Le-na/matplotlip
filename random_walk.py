4926d139ffd72f7737534e6829905c8daa9f8d4f
elena <zanozinaeo@gmail.com>
2023-07-23 18:23:35 +0300

    show many points

---
+++ random_walk.py
@@ -0,0 +1,35 @@
+from random import choice
+
+class RandomWalk():
+    """ Класс для генерирования слуйчайнных блуужданий"""
+
+    def __init__(self, num_points=5000):
+        """ Инициализирует атрибуты блужданий"""
+        self.num_points = num_points
+
+        # Все блуждания начинаются с точки (0, 0)
+        self.x_values = [0]
+        self.y_values = [0]
+
+    def fill_walk(self):
+        """ Вычисляет вссе точки блуждания"""
+        while len(self.x_values) < self.num_points:
+            # Определение направления и длины перемещения
+            x_direction = choice([1, -1])
+            x_distance = choice([0, 1, 2, 3, 4])
+            x_step = x_direction * x_distance
+
+            y_direction = choice([1, -1])
+            y_distance = choice([0, 1, 2, 3, 4])
+            y_step = y_direction * y_distance
+
+            # Отклонение нулевых перемещений
+            if x_step == 0 and y_step == 0:
+                continue
+
+            # Вычисленик следующих значений x и y
+            x = self.x_values[-1] + x_step
+            y = self.y_values[-1] + y_step
+
+            x = self.x_values.append(x)
+            y = self.y_values.append(y)
--- scatter_squares.py
+++ scatter_squares.py
@@ -1,11 +1,13 @@
 import matplotlib.pyplot as plt
 
 
-x_values = [1, 2, 3, 4, 5]
-y_values = [1, 4, 9, 16, 25]
+# x_values = [1, 2, 3, 4, 5]
+# y_values = [1, 4, 9, 16, 25]
+x_values = list(range(1, 5001))
+y_values = [x**3 for x in x_values]
 plt.style.use("seaborn-v0_8")    # Нанеесение на диаграмму отдельной точки используется функция scatter()
 fig, ax = plt.subplots()
-ax.scatter(x_values, y_values, s=100)
+ax.scatter(x_values, y_values, c=y_values, cmap=plt.cm.cool, s=10)
 
 # Назначение заголовка диаграммы и меток осей.
 ax.set_title("Square Numbers", fontsize=24)
@@ -15,4 +17,10 @@ ax.set_ylabel("Square of Value", fontsize=14)
 # Назначение размера шрифта делений на осях.
 ax.tick_params(axis='both', which='major', labelsize=14)
 
+# Назначение диапазона для каждой оси
+ax.axis([0, 5050, 0, max(y_values)])
 plt.show()
+# plt.savefig()   #Для автоматического сохранения в файл набрать этот код, вместо plt.show()
+# plt.savefig('squares_plot.png', bbox_inches='tight')
+# Первый аргумент содержит имя файла для сохранения диаграммы;
+# Второй аргумент отсекает от диаграммы лишнее пространство.
