a11dee55159a61e0b22a7011b7239c8893c3067b
elena <zanozinaeo@gmail.com>
2023-07-23 18:50:41 +0300

    upgrade random graph - the first and the last points

--- rw_visual.py
+++ rw_visual.py
@@ -10,7 +10,13 @@ while True:
     # Нанесение точек на диаграмму
     plt.style.use("classic")
     fig, ax = plt.subplots()
-    ax.scatter(rw.x_values, rw.y_values, s=15)
+    point_numbers = range(rw.num_points)
+    ax.scatter(rw.x_values, rw.y_values, c=point_numbers,
+               cmap=plt.cm.Blues, edgecolor='none', s=15)
+
+    # Выделение первой и последней точек
+    ax.scatter(0, 0, c="green", edgecolor='none', s=100)
+    ax.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolor='none', s=100)
     plt.show()
 
     keep_running = input("Make another walk? (y/n):")
