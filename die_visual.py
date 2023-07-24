c2b422e2590e1cfe16fdc2b9c6717d9998f9b793
elena <zanozinaeo@gmail.com>
2023-07-24 16:43:03 +0300

    upgrade random graph - added points

---
+++ die_visual.py
@@ -0,0 +1,11 @@
+from die import Die
+
+# Создание кубика D6
+die = Die()
+
+# Моделирование серии бросков с сохранением результатов в списке
+results = []
+for roll in range(100):
+    result = die.roll()
+    results.append(result)
+print(results)
