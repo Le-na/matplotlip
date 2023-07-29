e28758caf621a8264f01daf2febc63eb92ce3d42
elena <zanozinaeo@gmail.com>
2023-07-29 20:37:02 +0300

    used try-except-else

--- data_loading/death_valley_highs_low.py
+++ data_loading/death_valley_highs_low.py
@@ -28,12 +28,16 @@ with open(filename) as f:
     dates, highs, lows = [], [], []
     for row in reader:
         current_date = datetime.strptime(row[2], "%Y-%m-%d")
-        high = int(row[4])  #индексы обновляются в соответствии с позициями TMAX и TMIN в данном файле.
-        low = int(row[5])   #и тут
-        dates.append(current_date)
-        highs.append(high)
-        lows.append(low)
-    print(highs)
+        try:
+            high = int(row[4])  #индексы обновляются в соответствии с позициями TMAX и TMIN в данном файле.
+            low = int(row[5])   #и тут
+        except ValueError:
+            print(f"Missing data for {current_date}")
+        else:
+            dates.append(current_date)
+            highs.append(high)
+            lows.append(low)
+        print(highs)
 
 # Нанесение данных на диаграмму
 plt.style.use('seaborn')
@@ -43,7 +47,8 @@ ax.plot(dates, lows, c="blue", alpha=0.5)
 plt.fill_between(dates, highs, lows, facecolor='blue', alpha=0.1)
 
 # Форматирование диаграммы
-plt.title("Daily high low temperatures - 2018", fontsize=24)
+title = "Daily high low temperatures - 2018\nDeath Valley, CA"
+plt.title(title, fontsize=20)
 plt.xlabel('', fontsize=16)
 fig.autofmt_xdate()
 plt.ylabel('Temperature (F)', fontsize=16)
