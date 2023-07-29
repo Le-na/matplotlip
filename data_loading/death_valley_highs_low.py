a4c019e992c345a0c18a75ef76f65092c48d35eb
elena <zanozinaeo@gmail.com>
2023-07-29 20:30:02 +0300

    error output

--- data_loading/death_valley_highs_low.py
+++ data_loading/death_valley_highs_low.py
@@ -1,8 +1,52 @@
+# import csv
+# filename = 'data/death_valley_2018_simple.csv'
+# with open(filename) as f:
+#     reader = csv.reader(f)
+#     header_row = next(reader)
+#
+#     for index, column_header in enumerate(header_row):
+#         print(index, column_header)
+#
+
+
+
+
 import csv
+from datetime import datetime
+from matplotlib import pyplot as plt
+
 filename = 'data/death_valley_2018_simple.csv'
 with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
 
-    for index, column_header in enumerate(header_row):
-        print(index, column_header)
+
+    # for index, column_header in enumerate(header_row):
+    #     print(index, column_header)
+
+    # Получение дат, температурнх минимумов и максимумов из файла
+    dates, highs, lows = [], [], []
+    for row in reader:
+        current_date = datetime.strptime(row[2], "%Y-%m-%d")
+        high = int(row[4])  #индексы обновляются в соответствии с позициями TMAX и TMIN в данном файле.
+        low = int(row[5])   #и тут
+        dates.append(current_date)
+        highs.append(high)
+        lows.append(low)
+    print(highs)
+
+# Нанесение данных на диаграмму
+plt.style.use('seaborn')
+fig, ax = plt.subplots()
+ax.plot(dates, highs, c='red', alpha=0.5)
+ax.plot(dates, lows, c="blue", alpha=0.5)
+plt.fill_between(dates, highs, lows, facecolor='blue', alpha=0.1)
+
+# Форматирование диаграммы
+plt.title("Daily high low temperatures - 2018", fontsize=24)
+plt.xlabel('', fontsize=16)
+fig.autofmt_xdate()
+plt.ylabel('Temperature (F)', fontsize=16)
+plt.tick_params(axis='both', which='major', labelsize=16)
+
+plt.show()
