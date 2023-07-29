1cfa96b51eac03b053998aca414bb88a153eb52c
Elena <zanozinaeo@gmal.com>
2023-07-29 18:40:11 +0300

    add sitka_highs

---
+++ data_loading/sitka_highs.py
@@ -0,0 +1,30 @@
+import csv
+from matplotlib import pyplot as plt
+
+filename = 'data/sitka_weather_07-2018_simple.csv'
+with open(filename) as f:
+    reader = csv.reader(f)
+    header_row = next(reader)
+
+    # for index, column_header in enumerate(header_row):
+    #     print(index, column_header)
+
+    # Чтение максимальных температур
+    highs = []
+    for row in reader:
+        high = int(row[5])
+        highs.append(high)
+    print(highs)
+
+# Нанесение данных на диаграмму
+plt.style.use('seaborn')
+fig, ax = plt.subplots()
+ax.plot(highs, c='red')
+
+# Форматирование диаграммы
+plt.title("Daily high temperatures, July 2018", fontsize=24)
+plt.xlabel('', fontsize=16)
+plt.ylabel('Temperature (F)', fontsize=16)
+plt.tick_params(axis='both', which='major', labelsize=16)
+
+plt.show()
