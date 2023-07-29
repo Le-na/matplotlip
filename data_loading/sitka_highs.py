925196252dd876c9ba88e5db890e6d593d50c614
elena <zanozinaeo@gmail.com>
2023-07-29 20:15:08 +0300

    low(min, max) temperatures

--- data_loading/sitka_highs.py
+++ data_loading/sitka_highs.py
@@ -2,30 +2,34 @@ import csv
 from datetime import datetime
 from matplotlib import pyplot as plt
 
-filename = 'data/sitka_weather_07-2018_simple.csv'
+filename = 'data/sitka_weather_2018_simple.csv'
 with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
 
+
     # for index, column_header in enumerate(header_row):
     #     print(index, column_header)
 
-    # Чтение дат и максимальных температур
-    dates, highs = [], []
+    # Получение дат, температурнх минимумов и максимумов из файла
+    dates, highs, lows = [], [], []
     for row in reader:
         current_date = datetime.strptime(row[2], "%Y-%m-%d")
         high = int(row[5])
+        low = int(row[6])
         dates.append(current_date)
         highs.append(high)
+        lows.append(low)
     print(highs)
 
 # Нанесение данных на диаграмму
 plt.style.use('seaborn')
 fig, ax = plt.subplots()
 ax.plot(dates, highs, c='red')
+ax.plot(dates, lows, c="blue")
 
 # Форматирование диаграммы
-plt.title("Daily high temperatures, July 2018", fontsize=24)
+plt.title("Daily high low temperatures - 2018", fontsize=24)
 plt.xlabel('', fontsize=16)
 fig.autofmt_xdate()
 plt.ylabel('Temperature (F)', fontsize=16)
