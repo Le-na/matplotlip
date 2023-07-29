069ad260e321b4aa129dbb59bcce3f279b17d881
elena <zanozinaeo@gmail.com>
2023-07-29 20:22:39 +0300

    use arg facecolor and 'alpha=1/0.5/0.1'-is transparent color

--- data_loading/sitka_highs.py
+++ data_loading/sitka_highs.py
@@ -25,8 +25,9 @@ with open(filename) as f:
 # Нанесение данных на диаграмму
 plt.style.use('seaborn')
 fig, ax = plt.subplots()
-ax.plot(dates, highs, c='red')
-ax.plot(dates, lows, c="blue")
+ax.plot(dates, highs, c='red', alpha=0.5)
+ax.plot(dates, lows, c="blue", alpha=0.5)
+plt.fill_between(dates, highs, lows, facecolor='blue', alpha=0.1)
 
 # Форматирование диаграммы
 plt.title("Daily high low temperatures - 2018", fontsize=24)
